3d66bb6539e9f2c8fa2c0c7d8628603a9735a3de
bower-9e-ay2526 <bower.ae@obmontessori.edu.ph>
2026-04-10 09:58:58 +0800

    push all into main

---
+++ main.js
@@ -0,0 +1,20 @@
+function clickConfirm() {
+    var modes = Number(document.getElementById('mode').value);
+
+    switch(modes) {
+        case 1: 
+        document.getElementById("Level").innerHTML="Level: Easy Mode"; 
+        break;
+
+        case 2: 
+        document.getElementById("Level").innerHTML="Level: Medium Mode"; 
+        break;
+
+        case 3: 
+        document.getElementById("Level").innerHTML="Level: Hard Mode"; 
+        break;
+
+        default:
+            window.alert("Invalid Section Number");
+    }
+}
